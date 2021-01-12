4993f9c20a0c90f98f5838d7c34914636dadade8
JohnKarima <karimajohn24@gmail.com>
2021-01-12 11:49:39 +0300

    change api credentials

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from "react";
+import Recipe from "./Recipe"
 import './App.css';
 
 const App = () => {
-    const APP_ID = 'b0d54b4a';
-    const APP_KEY = '702918875c69e606e385d51247820f57';
+    const APP_ID = '20a2e689';
+    const APP_KEY = '459b4e395326595a60fae98334567886';
+
+    const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
         getRecipes();
@@ -11,16 +14,26 @@ const App = () => {
 
     const getRecipes = async () => {
         const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-        const data = response.json();
-        console.log(data);
+
+        const data = await response.json();
+        console.log(data.hits);
+        setRecipes(data.hits);
     }
 
     return(
         <div className="App">
             <form className="search-form">
                 <input className="search-bar" type="text"></input>
-                <button className="search-button" type="submit">Submit for recipe</button>
+                <button className="search-button" type="submit">Submit</button>
             </form>
+            {recipes.map(recipe => (
+                <Recipe 
+                title={recipe.recipe.label} 
+                calories={recipe.recipe.calories} 
+                image={recipe.reecipe.image}
+                />
+
+            ))}
         </div>
     )
 }
