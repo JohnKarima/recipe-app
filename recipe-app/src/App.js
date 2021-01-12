536ea4de48c26dc730769ceb763a782f9a61927d
username:moringa_school <mc_26@moringaschool.com>
2021-01-12 10:31:40 +0300

    project set-up

---
+++ recipe-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+    const APP_ID = 'b0d54b4a';
+    const APP_KEY = '702918875c69e606e385d51247820f57';
+
+    return(
+        <div className="App">
+            <h1>Hello React</h1>
+        </div>
+    )
+
+}
+
+export default App;
