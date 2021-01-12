67122e88e1b4575d1632ff48e1426db842ac3e13
username:moringa_school <mc_26@moringaschool.com>
2021-01-12 11:05:22 +0300

    Successfully comsume Edamam recipe API

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,18 +1,28 @@
-import React from "react";
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 const App = () => {
-
     const APP_ID = 'b0d54b4a';
     const APP_KEY = '702918875c69e606e385d51247820f57';
 
+    useEffect(() => {
+        getRecipes();
+    },[]);
+
+    const getRecipes = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = response.json();
+        console.log(data);
+    }
+
     return(
         <div className="App">
-            <h1>Hello React</h1>
+            <form className="search-form">
+                <input className="search-bar" type="text"></input>
+                <button className="search-button" type="submit">Submit</button>
+            </form>
         </div>
     )
-
 }
 
 export default App;
